ddbc31f83472a36d421c3eca597c72bcb213e05c
gissandrogama <gissandrogama@gmail.com>
2020-08-15 12:11:20 -0300

    mix

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule App.MixProject do
       {:phoenix, "~> 1.5.4"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.15.5"},
       {:phoenix_live_view, "~> 0.14.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
