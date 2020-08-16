1c953d10c92411a043e020d76a85f1cc1d50636b
gissandrogama <gissandrogama@gmail.com>
2020-08-16 20:51:10 -0300

    modifi prod, secret

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :app, AppWeb.Endpoint,
-  url: [host: "localhost", port: 4000],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
