929bb303a0117f1103b760604e4ec17de9636a98
gissandrogama <gissandrogama@gmail.com>
2020-08-16 12:57:44 -0300

    modifi to deploy gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -24,18 +24,18 @@ secret_key_base =
 # which you should run after static files are built and
 # before starting your production server.
 config :app, AppWeb.Endpoint,
-  load_from_system_env: true,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pesquise-liaderes.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [host: "pesquise-lideres.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: secret_key_base
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
 
 # Configure your database
 config :app, AppWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
   ssl: true,
-  url: database_url,
+  pool_size: 2
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # Do not print debug messages in production
